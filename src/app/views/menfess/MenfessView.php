f31d8d502fd59a4acc033b8aa1fe5b8489b3164e
yobeldc <13521067@std.stei.itb.ac.id>
2023-11-17 05:50:43 +0700

    feat: add menfess

---
+++ src/app/views/menfess/MenfessView.php
@@ -0,0 +1,14 @@
+<?php
+
+class MenfessView
+{
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function render(): void
+    {
+        require_once __DIR__ . "/../../component/menfess/MenfessComponent.php";
+    }
+}
