d256692bfd18075f936049597260513a01f12606
yobeldc <13521067@std.stei.itb.ac.id>
2023-11-17 05:51:31 +0700

    feat: add studio

--- src/app/controllers/StudioController.php
+++ src/app/controllers/StudioController.php
@@ -6,12 +6,30 @@ class StudioController
         try {
             switch ($_SERVER['REQUEST_METHOD']) {
                 case 'GET':
+                    // Authentication
+                    $auth = Utils::middleware("Authentication");
+                    $auth->isUserLogin();
+                    $data['isLogin'] = true;
+
+                    $isAdmin = false; 
+                    try {
+                        $auth->isAdminLogin();
+                        $isAdmin = true;
+                    } catch (Exception $e) {
+                        if ($e-> getCode() !== STATUS_UNAUTHORIZED) {
+                            throw new Exception($e->getMessage(), $e->getCode());
+                        }
+                    }
+
+                    $data["isAdmin"] = $isAdmin;
                     var_dump(SOAP_API_URL);
 
                     $client = Utils::client("SoapServiceClient");
                     var_dump($client);
                     $client->invoke("Example", array('input' => [12, 13, 14]));
 
+                    $studioAllView = Utils::view("studio", "StudioAllView", $data);
+                    $studioAllView->render();
                     break;
                 default:
                     throw new Exception('Method Not Allowed', STATUS_METHOD_NOT_ALLOWED);
@@ -22,6 +40,50 @@ class StudioController
             } else {
                 http_response_code($e->getCode());
             }
+        }
+    }
+
+    public function detail(){
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    // Authentication
+                    $auth = Utils::middleware("Authentication");
+                    $auth->isUserLogin();
+                    $data['isLogin'] = true;
+
+                    $isAdmin = false; 
+                    try {
+                        $auth->isAdminLogin();
+                        $isAdmin = true;
+                    } catch (Exception $e) {
+                        if ($e-> getCode() !== STATUS_UNAUTHORIZED) {
+                            throw new Exception($e->getMessage(), $e->getCode());
+                        }
+                    }
+
+                    $data["isAdmin"] = $isAdmin;
+
+                    var_dump(SOAP_API_URL);
+
+                    $client = Utils::client("SoapServiceClient");
+                    var_dump($client);
+                    $client->invoke("Example", array('input' => [12, 13, 14]));
+
+                    $studioView = Utils::view("studio", "StudioView", $data);
+                    $studioView->render();
+                    break;
+                default:
+                    throw new Exception('Method Not Allowed', STATUS_METHOD_NOT_ALLOWED);
+            }
+        } catch (Exception $e) {
+            if ($e->getCode() === STATUS_UNAUTHORIZED) {
+                header("Location: http://localhost:8080/user/login");
+            } else {
+                http_response_code($e->getCode());
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
         }
     }
 }
---
+++ src/app/views/studio/StudioView.php
@@ -0,0 +1,14 @@
+<?php
+
+class StudioView
+{
+    public function __construct($data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function render(): void
+    {
+        require_once __DIR__ . "/../../component/studio/StudioComponent.php";
+    }
+}
